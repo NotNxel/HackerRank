683f196e1d79911b80bca435b460c81fc9612f13
Neel Nikhil <notnxel@gmail.com>
2025-01-04 00:16:19 +0530

    Create Say 'Hello, World!' With Python.py

---
+++ Python/Introduction/Say 'Hello, World!' With Python.py
@@ -0,0 +1,15 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+# Difficulty: Easy
+# Max Score: 5
+# Language: Python
+# Credit : Neel Nikil
+
+# ========================
+#         Solution
+# ========================
+
+print("Hello, World!")

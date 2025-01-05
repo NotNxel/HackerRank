0fa1c4f8ca116d0f9c480bc400b9fb1d0ed0a051
Neel Nikhil <notnxel@gmail.com>
2025-01-06 00:07:03 +0530

    Print_Function.py

---
+++ Python/Introduction/Print_Function.py
@@ -0,0 +1,18 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: www.hackerrank.com/challenges/write-a-function/problem
+# Difficulty: Medium
+# Max Score: 20
+# Language: Python
+# Credit : Neel Nikil Pappu
+
+# ========================
+#         Solution
+# ========================
+
+if __name__ == '__main__':
+    n = int(input())
+    for a in range (1,n+1):
+        print(a,end='',sep='')

e17b07bb598cca86c239f5984d19f0c494ee8e31
Neel Nikhil <notnxel@gmail.com>
2025-01-05 23:58:46 +0530

    Python: Division.py

---
+++ Python/Introduction/Python: Division.py
@@ -0,0 +1,19 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/python-division/problem
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+# Credit : Neel Nikil Pappu
+
+# ========================
+#         Solution
+# ========================
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)

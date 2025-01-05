f6dc917e4c8b7799dc9a23ec4933d7587cf1dabf
Neel Nikhil <notnxel@gmail.com>
2025-01-06 00:00:44 +0530

    Loops.py

---
+++ Python/Introduction/Loops.py
@@ -0,0 +1,18 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/python-loops/problem
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+# Credit : Neel Nikil Pappu
+
+# ========================
+#         Solution
+# ========================
+
+if __name__ == '__main__':
+    n = int(input())
+    for a in range(0,n):
+        print(a*a)

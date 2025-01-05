f84a4e729cfe1879fb4e4622ed683e8fbf24255e
Neel Nikhil <notnxel@gmail.com>
2025-01-06 00:04:27 +0530

    Write_a_Function.py

---
+++ Python/Introduction/Write_a_Function.py
@@ -0,0 +1,29 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: www.hackerrank.com/challenges/write-a-function/problem
+# Difficulty: Medium
+# Max Score: 10
+# Language: Python
+# Credit : Neel Nikil Pappu
+
+# ========================
+#         Solution
+# ========================
+
+def is_leap(year):
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+                leap=True
+            else:
+                leap= False
+        else:
+            leap=True
+    else:
+        leap=False
+    return leap
+
+year = int(input())
+print(is_leap(year))

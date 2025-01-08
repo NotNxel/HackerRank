b439a90c13b31159aa15ebeb7939e04758efa4ea
Neel Nikhil <notnxel@gmail.com>
2025-01-08 23:10:39 +0530

    Lists.py

---
+++ Python/Basic Data Types/Lists.py
@@ -0,0 +1,43 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/python-lists/problem
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+# Credit : Neel Nikhil Pappu
+
+# ========================
+#         Solution
+# ========================
+
+if __name__ == '__main__':
+    N = int(input())
+    l = []
+for i in range(N):
+    name = input()
+    spl = str.split(name)
+    if len(spl) == 3:
+        com = str(spl[0])
+        ind = int(spl[1])
+        obj = int(spl[2])
+    elif len(spl) == 2:
+        com = str(spl[0])
+        obj = int(spl[1])
+    elif len(spl) == 1:
+        com = str(spl[0])
+    if com == "insert":
+        l.insert(ind,obj)
+    elif com == "print":
+        print (l)
+    elif com == "remove":
+        l.remove(obj)
+    elif com == "append":
+        l.append(obj)
+    elif com == "sort":
+        l.sort()
+    elif com == "pop":
+        l.pop()
+    else:
+        l.reverse()

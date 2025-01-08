ac1f1b8acf3c6576dfe0d841444e7b4ad2ed3840
Neel Nikhil <notnxel@gmail.com>
2025-01-08 23:04:55 +0530

    Finding_The_Percentage.py

---
+++ Python/Basic Data Types/Finding_The_Percentage.py
@@ -0,0 +1,23 @@
+# ========================
+#       Information
+# ========================
+
+# Direct Link: https://www.hackerrank.com/challenges/list-comprehensions/problem
+# Difficulty: Easy
+# Max Score: 10
+# Language: Python
+# Credit : Neel Nikhil Pappu
+
+# ========================
+#         Solution
+# ========================
+
+N = input();
+
+map = {};
+for i in range(0,int(N)):
+    S = input().split();
+    lst = (float(S[1]) + float(S[2]) + float(S[3]))/3.0;
+    map[S[0]] = lst;
+
+print("%0.2f" %map[input()]);

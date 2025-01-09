8bed16415e007f59242092e71db5e108a7d12991
Neel Nikhil <notnxel@gmail.com>
2025-01-09 21:57:36 +0530

    Select_All.sql

--- SQL/Basic Select/Select_All.sql
+++ SQL/Basic Select/Select_All.sql
@@ -1,1 +1,15 @@
+-- ========================
+--       Information
+-- ========================
 
+-- Direct Link: https://www.hackerrank.com/challenges/select-all-sql/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: mySQL
+-- Credit : Neel Nikhil Pappu
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT * from city;

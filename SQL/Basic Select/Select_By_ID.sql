fc13d9aa6b98f24bff2cd8f2f25fc9bbf733c019
Neel Nikhil <notnxel@gmail.com>
2025-01-09 22:01:33 +0530

    Select_By_ID.sql

---
+++ SQL/Basic Select/Select_By_ID.sql
@@ -0,0 +1,15 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/select-by-id/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: mySQL
+-- Credit : Neel Nikhil Pappu
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT * FROM CITY where Id='1661' ;

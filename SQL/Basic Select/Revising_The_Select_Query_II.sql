40be78ba7ad6884ce91f169fca3af47c728082a0
Neel Nikhil <notnxel@gmail.com>
2025-01-09 21:55:24 +0530

    Revising_The_Select_Query_II.sql

---
+++ SQL/Basic Select/Revising_The_Select_Query_II.sql
@@ -0,0 +1,15 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- Difficulty: Easy
+-- Max Score: 10
+-- DBMS: mySQL
+-- Credit : Neel Nikhil Pappu
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT name FROM CITY WHERE population > 120000 AND Countrycode ="USA";
